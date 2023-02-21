abe64c0398b116915c84f9fa06b6d042b2cf2625
Marcelo Junior <marcelo.junior@escoladnc.com.br>
2023-02-21 16:28:52 -0300

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+var ImgLampada = document.getElementById('ImgLampada');
+
+function ligar(){
+    ImgLampada.src = './img/acesa.png'
+}
+
+function desligar(){
+    ImgLampada.src = './img/lampada.png'
+}
+
+function quebrada(){
+    ImgLampada.src = './img/quebrada.png'
+}
